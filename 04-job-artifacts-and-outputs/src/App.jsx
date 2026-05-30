25bdab9fb19076e4d5ac2a09a9e1ff09cfc47087
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2026-05-30 15:05:56 +0545

    75. using & invalidating caches

--- 04-job-artifacts-and-outputs/src/App.jsx
+++ 04-job-artifacts-and-outputs/src/App.jsx
@@ -2,17 +2,17 @@ import MainContent from './components/MainContent';
 import logo from './assets/images/logo.png';
 
 function App() {
-  return (
-    <>
-      <header>
-        <div id="logo-img">
-          <img src={logo} />
-        </div>
-        <h1>Learn & Master GitHub Actions</h1>
-      </header>
-      <MainContent />
-    </>
-  );
+	return (
+		<>
+			<header>
+				<div id="logo-img">
+					<img src={logo} />
+				</div>
+				<h2>Learn & Master GitHub Actions</h2>
+			</header>
+			<MainContent />
+		</>
+	);
 }
 
 export default App;
